83091cce3f8a8b397e13e6eb719fc87fc53c86a8
zs5460 <zs5460@gmail.com>
2019-09-21 10:25:32 +0800

    test:

--- art_test.go
+++ art_test.go
@@ -13,6 +13,8 @@ func TestString(t *testing.T) {
 		args args
 		want string
 	}{
+		{name: "not exist", args: args{text: "\n"}, want: " \n \n \n \n \n "},
+		{name: "null", args: args{text: ""}, want: ""},
 		{name: "zs", args: args{text: "zs"}, want: `             
  ____   _____
 /_  /  / ___/
