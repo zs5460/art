e00069349c6fc9dcded9997a70b713e293bcdbbe
zs5460 <zs5460@gmail.com>
2019-09-24 13:43:58 +0800

    update:main.go add wtite to file

--- cmd/art/main.go
+++ cmd/art/main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"flag"
 	"fmt"
+    "io/ioutil"
 
-	"github.com/zs5460/art"
+    "github.com/zs5460/art"
 )
 
-var s string
+var (
+	s string
+	w bool
+)
 
 func main() {
-	flag.StringVar(&s, "text", "art - zs5460", "")
-	flag.Parse()
-	fmt.Println(art.String(s))
+    flag.StringVar(&s, "s", "art - zs5460", "the string to be converted ")
+    flag.BoolVar(&w, "w", false, "write result to file ")
+    flag.Parse()
+
+    banner := art.String(s)
+    if w {
+        code := fmt.Sprintf("banner := `\n%s\n`",banner)
+        _ = ioutil.WriteFile("banner.txt",[]byte(code),0666)
+    }else{
+        fmt.Println(banner)
+    }
 }
